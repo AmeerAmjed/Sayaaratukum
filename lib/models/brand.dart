6bba69a7c8eaba9c1ae0afeed39bd4c5128ba4eb
ameeramjed <pr.ameer98@gmail.com>
2023-02-28 15:54:45 +0300

    add brand model

---
+++ lib/models/brand.dart
@@ -0,0 +1,27 @@
+import 'model.dart';
+
+class BrandModel extends BaseModel {
+  BrandModel({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  final int id;
+  final String title;
+  final String imageUrl;
+
+  factory BrandModel.fromJson(Map<String, dynamic> data) {
+    return BrandModel(
+      id: data['id'],
+      title: data['name'],
+      imageUrl: data['image'],
+    );
+  }
+
+  static List<BrandModel> listFromJson(list) => List<BrandModel>.from(
+        list.map(
+          (x) => BrandModel.fromJson(x),
+        ),
+      ).toList();
+}
