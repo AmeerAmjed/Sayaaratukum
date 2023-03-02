660f0a60d253ec9bd3590c2c03d2738acb0f013f
ameeramjed <pr.ameer98@gmail.com>
2023-03-02 18:58:03 +0300

    add Vertical Space

---
+++ lib/widgets/vertical_space.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:sayaaratukum/util/constant.dart';
+
+class VerticalSpace4 extends StatelessWidget {
+  const VerticalSpace4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: Constants.spacingSmall,
+    );
+  }
+}
+
+class VerticalSpace8 extends StatelessWidget {
+  const VerticalSpace8({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: Constants.spacingXSmall,
+    );
+  }
+}
+
+class VerticalSpace12 extends StatelessWidget {
+  const VerticalSpace12({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: Constants.spacingXXSmall,
+    );
+  }
+}
+
+class VerticalSpace16 extends StatelessWidget {
+  const VerticalSpace16({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: Constants.spacingMedium,
+    );
+  }
+}
+
+class VerticalSpace24 extends StatelessWidget {
+  const VerticalSpace24({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: Constants.spacingLarge,
+    );
+  }
+}
+
+class VerticalSpace32 extends StatelessWidget {
+  const VerticalSpace32({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 32.0,
+    );
+  }
+}
+
+class VerticalSpace45 extends StatelessWidget {
+  const VerticalSpace45({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 45.0,
+    );
+  }
+}
