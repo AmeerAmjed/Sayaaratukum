ba8317aca5de1581d959e01ed5f2b4b14a03b683
ameeramjed <pr.ameer98@gmail.com>
2023-03-02 18:56:37 +0300

    add Horizontal Space

---
+++ lib/widgets/horizontal_space.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:sayaaratukum/util/constant.dart';
+
+class HorizontalSpace4 extends StatelessWidget {
+  const HorizontalSpace4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: Constants.spacingSmall,
+    );
+  }
+}
+
+class HorizontalSpace8 extends StatelessWidget {
+  const HorizontalSpace8({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: Constants.spacingXSmall,
+    );
+  }
+}
+
+class HorizontalSpace12 extends StatelessWidget {
+  const HorizontalSpace12({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: Constants.spacingMedium,
+    );
+  }
+}
+
+class HorizontalSpace16 extends StatelessWidget {
+  const HorizontalSpace16({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: Constants.spacingXXSmall,
+    );
+  }
+}
+
+class HorizontalSpace20 extends StatelessWidget {
+  const HorizontalSpace20({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: Constants.spacingXMedium,
+    );
+  }
+}
