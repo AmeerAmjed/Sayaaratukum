4f028203e832fe2ba59f7eede7d9052a1beb8ccd
ameeramjed <pr.ameer98@gmail.com>
2023-03-03 23:16:03 +0300

    add box

---
+++ lib/widgets/box.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sayaaratukum/util/constant.dart';
+
+class Box extends StatelessWidget {
+  const Box({
+    Key? key,
+    required this.size,
+    required this.child,
+  }) : super(key: key);
+
+  final double size;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          Constants.radiusSmall,
+        ),
+      ),
+      child: Card(
+        child: child,
+      ),
+    );
+  }
+}
