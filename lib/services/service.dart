0a601a53f0a4c02963c8d29c67e90ff43d809827
AmeerAmjed <pr.ameer98@gmail.com>
2023-03-07 17:50:23 +0300

    add base service

---
+++ lib/services/service.dart
@@ -0,0 +1,27 @@
+import 'package:get/get_connect/connect.dart';
+import 'package:get/get_connect/http/src/request/request.dart';
+import 'package:sayaaratukum/controllers/application.dart';
+import 'package:sayaaratukum/services/api.dart';
+import 'package:sayaaratukum/services/local/storage.dart';
+
+abstract class Service extends GetConnect with LocalStorage {
+  String? token = Application.instance.token.value;
+
+  @override
+  void onInit() {
+    httpClient.baseUrl = ApiEndpoint.baseUrl;
+    httpClient.timeout = const Duration(seconds: 3000);
+    httpClient.defaultContentType = "application/json";
+    httpClient.addResponseModifier((request, response) async {
+      print("Service ${response.body}");
+      return response;
+    });
+
+    httpClient.addRequestModifier((Request request) {
+      request.headers['Authorization'] = "Bearer $token";
+      return request;
+    });
+
+    super.onInit();
+  }
+}
