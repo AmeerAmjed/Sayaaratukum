eeee70b6e65d1115131ef5f523953a153fdd410e
ameeramjed <pr.ameer98@gmail.com>
2023-03-15 20:43:38 +0300

    add button

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:sayaaratukum/theme/theme.dart';
+
+class Buttons extends StatelessWidget {
+  const Buttons({
+    Key? key,
+    this.label,
+    this.icon,
+    this.background,
+    this.onPressed,
+    this.height = 56.0,
+    this.width = 30.0,
+    this.disable = false,
+    this.fullBackground = true,
+    // this.colorIcon = ThemeApp.primaryColor,
+  }) : super(key: key);
+
+  final String? label;
+  final double height;
+  final double width;
+  final bool disable;
+  final bool fullBackground;
+  final Color? background;
+  final VoidCallback? onPressed;
+  final IconData? icon;
+
+  // final Color colorIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: EdgeInsets.zero,
+      margin: EdgeInsets.zero,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(EdgeInsets.zero),
+          elevation: MaterialStateProperty.all(0.0),
+          backgroundColor: MaterialStateProperty.all(
+            fullBackground
+                ? (background ?? Theme.of(context).primaryColor)
+                : Colors.transparent,
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(4.0),
+              ),
+              side: fullBackground
+                  ? const BorderSide()
+                  : BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+            ),
+          ),
+        ),
+        onPressed: disable ? null : onPressed,
+        child: icon == null
+            ? disable
+                ? const SizedBox(
+                    height: 30.0,
+                    width: 30.0,
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  )
+                : Text(
+                    label!,
+                    style: ThemeApp.primaryTextStyle.copyWith(
+                        // color: fullBackground ? Colors.white : ThemeApp.primaryColor,
+                        ),
+                  )
+            : Icon(
+                icon,
+                // color: colorIcon,
+              ),
+      ),
+    );
+  }
+}
