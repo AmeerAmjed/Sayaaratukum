7d9b4963b3dc5b3d44cb31deffe49a6e5e9f5aa6
ameeramjed <pr.ameer98@gmail.com>
2023-06-05 05:16:43 +0300

    translate app

--- lib/l10n/ar.dart
+++ lib/l10n/ar.dart
@@ -17,6 +17,157 @@ class ArTranslations {
         L10n.socialMedia: 'التواصل الاجتماعي',
         L10n.logout: 'تسجيل خروج',
 
+
+        L10n.welcomeTitle: 'لنبدأ تجربة جديدة',
+        L10n.welcomeDoc: 'اختر سيارتك المفضلة، اشتريها واستمتع بتجربة جديدة!',
+        L10n.letStart : 'لنبدأ',
+
+        L10n.skip : 'تخطى',
+        L10n.login : 'تسجيل الدخول',
+        L10n.register : 'إنشاء حساب',
+        L10n.email : 'البريد الإلكتروني',
+        L10n.phoneNumber : 'رقم الهاتف',
+        L10n.firstName : 'الاسم الأول',
+        L10n.lastName : 'الاسم الأخير',
+
+
+        L10n.alreadyHaveAccount : 'هل لديك حساب؟',
+        L10n.createAccount : 'إنشاء حساب',
+        L10n.forgotPassword : 'نسيت كلمة المرور؟',
+        L10n.emailOrNumberPhone : 'البريد الإلكتروني أو رقم الهاتف',
+        L10n.isRequired: 'مطلوب',
+        L10n.emailIsInvalid: 'البريد الإلكتروني غير صالح',
+        L10n.passwordShort: 'يجب أن تكون كلمة المرور القصيرة أكثر من 8 أحرف',
+        L10n.passwordLong: 'يجب أن تكون كلمة المرور الطويلة أقل من 50 حرفًا',
+        L10n.passwordNotMatch: 'كلمة المرور غير متطابقة',
+
+        L10n.searchCar: 'ابحث عن سيارتك',
+        L10n.searchTool: 'ابحث عن أداة',
+
+        L10n.showrooms: 'صالات العرض',
+        L10n.adsDeals: 'عروض خاصة بالنسبة لك',
+        L10n.specifications: 'المواصفات',
+        L10n.note: 'ملاحظة',
+        L10n.chatToBuy: 'الدردشة للشراء',
+
+        L10n.make: 'الماركة',
+        L10n.model: 'الموديل',
+        L10n.status: 'الحالة',
+        L10n.serialNumber: 'الرقم التسلسلي',
+        L10n.color: 'اللون',
+        L10n.madeTo: 'مصنوع لـ',
+
+        L10n.showMore: 'عرض المزيد',
+        L10n.showLess: 'عرض أقل',
+        L10n.addCar: 'إضافة سيارة',
+        L10n.addTool: 'إضافة أداة',
+
+        L10n.back: 'رجوع',
+        L10n.next: 'التالي',
+        L10n.publish: 'نشر',
+
+        L10n.typeCar: 'نوع السيارة',
+        L10n.numberRegisterCar: 'رقم تسجيل السيارة',
+        L10n.propulsionSystem: 'نظام الدفع',
+        L10n.year: 'السنة',
+        L10n.fromYear: 'من السنة',
+        L10n.toYear: 'إلى السنة',
+
+        L10n.yearBrand: 'السنة مطلوبة!',
+
+        L10n.recolor: 'تغيير اللون',
+        L10n.km: 'كم',
+        L10n.no: 'لا',
+        L10n.yes: 'نعم',
+        L10n.automatic: 'أوتوماتيك',
+        L10n.manual: 'يدوي',
+        L10n.shakeCheck: 'فحص الهزات',
+        L10n.gearBox: 'صندوق التروس',
+        L10n.isDamage: 'ضرر السيارة',
+
+
+        L10n.governorate: 'المحافظة',
+        L10n.region: 'المنطقة',
+        L10n.nearPoint: 'نقطة قرب',
+        L10n.engineCapacity: 'سعة المحرك',
+        L10n.enginePower: 'قوة المحرك',
+        L10n.drivingMiles: 'عدد الأميال المقطوعة',
+
+        L10n.informationCar: 'معلومات السيارة',
+        L10n.locationCar: 'موقع السيارة',
+        L10n.imagesCar: 'الصور والسعر',
+        L10n.price: 'السعر',
+        L10n.minPrice: 'أدنى سعر',
+        L10n.maxPrice: 'أعلى سعر',
+        L10n.addImagesCar: 'إضافة عشر صور للسيارة',
+        L10n.addImageTool: 'إضافة صورة الأداة',
+        L10n.available: 'متاح',
+
+        L10n.failedCreateAccount: 'فشل إنشاء الحساب',
+
+        L10n.statusTool: 'حالة الأداة',
+        L10n.name: 'الاسم',
+        L10n.category: 'الفئة',
+        L10n.imagesTool: 'صورة الأداة',
+        L10n.description: 'الوصف',
+        L10n.newTool: 'جديد',
+        L10n.usedTool: 'مستعمل',
+        L10n.failedPublishTool: 'فشل نشر الأداة',
+        L10n.success: 'نجاح',
+        L10n.successAddTool: 'تمت إضافة الأداة بنجاح',
+
+        L10n.loginToAddFavorites: 'تسجيل الدخول لإضافة العناصر كمفضلة.',
+
+        L10n.empty: 'فارغ',
+        L10n.emptyFavorite: 'ابدأ بإضافة العناصر كمفضلة لرؤيتها هنا',
+
+
+        L10n.cancel: 'إلغاء',
+        L10n.areYouSure: 'هل أنت متأكد؟',
+        L10n.messageDeleteAccount: 'هل ترغب حقًا في حذف حسابك؟ لن تتمكن من التراجع عن هذا الإجراء.',
+        L10n.messageLogout: 'هل أنت متأكد من رغبتك في تسجيل الخروج؟',
+
+        L10n.notAuth: 'يرجى تسجيل الدخول / التسجيل',
+
+        L10n.successAddFavorite: 'تمت إضافة المفضلة بنجاح',
+        L10n.successRemoveFavorite: 'تمت إزالة المفضلة بنجاح',
+        L10n.failedAddFavorite: 'فشل إضافة المفضلة',
+
+        L10n.check: 'تحقق',
+        L10n.option: 'خيار',
+
+        L10n.subscribe: 'اشتراك',
+        L10n.subscribed: 'مشترك',
+        L10n.mySubscribed: 'مشترك لدي',
+        L10n.cars: 'السيارات',
+        L10n.tools: 'الأدوات',
+        L10n.now: 'الآن',
+
+
+        L10n.emptySubscribed: 'لم يتم الاشتراك في أي متجر بعد',
+        L10n.noteEmptySubscribed: 'ابدأ الاشتراك في المتاجر وستظهر جميع المتاجر المشترك فيها هنا',
+
+
+        L10n.filterBy: 'تصفية حسب',
+        L10n.cleanFilter: 'مسح الكل',
+        L10n.showResult: 'عرض النتائج',
+        L10n.noResult: 'لا توجد نتائج',
+
+        L10n.whatsappNotInstall: 'تطبيق WhatsApp غير مثبت على الجهاز. قم بتثبيت التطبيق وحاول مرة أخرى.',
+        L10n.numberPhoneNotAdd: 'لم يتم إضافة رقم الهاتف.',
+        L10n.whatsappNumberPhoneNotAdd: 'لم يتم إضافة رقم الهاتف لـ WhatsApp',
+
+        L10n.storeExpires: 'انتهت صلاحية المتجر',
+        L10n.notFound: 'غير موجود',
+        L10n.profile: 'الملف الشخصي',
+
+
+
+
+        "USA" : 'USA',
+        "Europe" : 'Europe',
+        "Gulf" : 'Gulf',
+
         "USA" : 'امريكي',
         "Europe" : 'اوربي',
         "Gulf" : 'خليجي',
