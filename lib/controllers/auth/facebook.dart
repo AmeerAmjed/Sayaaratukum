a3eadb3a971cdc079445e68b3ed0435a233a7996
ameeramjed <pr.ameer98@gmail.com>
2023-06-15 09:34:48 +0300

    setup login facebook

---
+++ lib/controllers/auth/facebook.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:sayaaratukum/controllers/controller.dart';
+
+class FacebookController extends BaseController {
+
+  late FacebookAuth facebookAuth;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  init() async {}
+
+  login() async {}
+}
