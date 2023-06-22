bd903cd4fb10a6457628d36ac4cdfe577b0176df
ameeramjed <pr.ameer98@gmail.com>
2023-06-22 03:10:37 +0300

    add model Notification

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -1,7 +1,34 @@
-import 'package:sayaaratukum/models/car.dart';
-
 import 'model.dart';
 
 class NotificationModel extends BaseModel {
+  final int id;
+  final String state;
+  final Object product;
+  final String? type;
+  final bool isRead;
 
+  NotificationModel({
+    required this.id,
+    required this.state,
+    required this.product,
+    this.type,
+    required this.isRead,
+  });
+
+  factory NotificationModel.fromJson(Map<String, dynamic> json) {
+    return NotificationModel(
+      id: json['id'],
+      state: json['state'],
+      product: json['product'],
+      type: json['type'],
+      isRead: json['isRead'],
+    );
+  }
+
+  static List<NotificationModel> listFromJson(list) =>
+      List<NotificationModel>.from(
+        list.map(
+          (x) => NotificationModel.fromJson(x),
+        ),
+      ).toList();
 }
